0b5b8634429af03b14d8e17422d527d1ea0b3d8a
Mulori <murilogarcia.hr@gmail.com>
2022-12-30 18:02:00 -0300

    update

--- src/server.js
+++ src/server.js
@@ -5,11 +5,21 @@ var cors = require("cors");
 
 const app = express();
 
+//Liberar o Cors
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "*");
-  app.use(cors());
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+    return res.status(200).json({});
+  }
   next();
 });
 
@@ -17,4 +27,4 @@ app.use(express.json());
 app.use(auth);
 app.use(conta);
 
-app.listen(7412, () => console.log("Server is running..."));
+app.listen(7412, () => console.log("O Serviço ZVIX está em execução..."));
