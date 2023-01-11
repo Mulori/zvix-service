6a9da596c2964cbe99a75fb4450288223faaa4c9
Mulori <murilogarcia.hr@gmail.com>
2023-01-11 00:30:29 -0300

    update

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const auth = require("./routes/auth");
 const conta = require("./routes/conta");
+const organizacao = require("./routes/organizacao");
+const controle = require("./routes/controle");
 var cors = require("cors");
 
 const app = express();
@@ -26,5 +28,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(auth);
 app.use(conta);
+app.use(organizacao);
+app.use(controle);
 
 app.listen(7412, () => console.log("O Serviço ZVIX está em execução..."));
